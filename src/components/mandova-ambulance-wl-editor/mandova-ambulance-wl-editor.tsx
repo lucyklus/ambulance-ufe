80ad88c40e72a45e5c8dfdc6a607a0deb51a278a
Lucia Mandová <lucka.mandova@gmail.com>
2024-03-04 14:50:40 +0100

    Add onClick method

--- src/components/mandova-ambulance-wl-editor/mandova-ambulance-wl-editor.tsx
+++ src/components/mandova-ambulance-wl-editor/mandova-ambulance-wl-editor.tsx
@@ -169,7 +169,7 @@ export class MandovaAmbulanceWlEditor {
 
         <md-divider></md-divider>
         <div class="actions">
-          <md-filled-tonal-button id="delete" disabled={!this.entry || this.entry?.id === '@new'}>
+          <md-filled-tonal-button id="delete" disabled={!this.entry || this.entry?.id === '@new'} onClick={() => this.deleteEntry}>
             <md-icon slot="icon">delete</md-icon>
             Zmazať
           </md-filled-tonal-button>
